e10af1a0b7128247e3069d6c7423519f4cd1bcc9
WeiXING213 <xingsir@gmail.com>
2018-04-04 15:58:28 +0200

    gsoap configuration wsdl and xsd files

---
+++ QtGuiApplication5/QtGuiApplication5/gsoap_generation_files/wsdl2.bat
@@ -0,0 +1,1 @@
+wsdl2h.exe -t typemap.dat -o onvif.h accesscontrol.wsdl accessrules.wsdl actionengine.wsdl advancedsecurity.wsdl analytics.wsdl analyticsdevice.wsdl credential.wsdl deviceio.wsdl devicemgmt.wsdl display.wsdl doorcontrol.wsdl event.wsdl imaging.wsdl media.wsdl media2.wsdl provisioning.wsdl ptz.wsdl receiver.wsdl recording.wsdl replay.wsdl schedule.wsdl search.wsdl thermal.wsdl
