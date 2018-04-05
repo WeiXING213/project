2b1062c0fc525f3b1599a1221b0a851eadb041bc
WeiXING213 <xingsir@gmail.com>
2018-04-05 11:46:35 +0200

    output proxy file with -j c++

--- QtGuiApplication5/QtGuiApplication5/gsoap_generation_files/soapcpp2.bat
+++ QtGuiApplication5/QtGuiApplication5/gsoap_generation_files/soapcpp2.bat
@@ -1,1 +1,1 @@
-soapcpp2.exe -2 -x -I./import;"C:\Users\DEV\source\project\onvif website file\gsoap_2.8.65\gsoap-2.8\gsoap" onvif.h
+soapcpp2.exe -j -2 -x -I./import;"C:\Users\DEV\source\project\onvif website file\gsoap_2.8.65\gsoap-2.8\gsoap" onvif.h
